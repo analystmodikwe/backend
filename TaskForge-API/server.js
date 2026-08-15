02edc5d12d193e3e02b7ca9048b89c42e087f212
lesedi <analystmodikwe18@gmail.com>
2026-08-15 18:29:34 +0200

    wrapping up the server

--- TaskForge-API/server.js
+++ TaskForge-API/server.js
@@ -7,3 +7,13 @@ const router = require("./routes/taskRoutes");
 
 // parsing JSON
 app.use(express.json());
+
+// use the router for a routes with API
+app.use("/api", router);
+
+//  statrtring the server with a port 3500
+const PORT = 3500
+
+app.listen(PORT, () =>{
+    console.log(`this server is running on http://localhost:${PORT}/api/tasks`)
+});
