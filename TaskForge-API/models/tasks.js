2d513ed69169acdf9b8eaeb6f6f8a21d261fe481
lesedi <analystmodikwe18@gmail.com>
2026-08-15 23:45:10 +0200

    adding tasks for testing

--- TaskForge-API/models/tasks.js
+++ TaskForge-API/models/tasks.js
@@ -1,3 +1,24 @@
-const tasks = [];
+const { nanoid } = require("nanoid");
+
+// seeded data so that i have something to test and verify on
+const tasks = [
+
+    // task 1
+    {
+        id: nanoid(),
+        title: "Add animations to the app",
+        completed: false,
+        createdAt: new Date().toISOString,
+    },
+
+    // task2 with missing tite on purpose to see if the error handling actually works or not
+        {
+        id: nanoid(),
+        
+        completed: false,
+        createdAt: new Date().toISOString,
+    },
+
+];
 
 module.exports = tasks;
