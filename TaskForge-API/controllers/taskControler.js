f1e48f34071e82553ccc63ba702adb76cee05fc8
lesedi <analystmodikwe18@gmail.com>
2026-08-15 21:44:11 +0200

    adding update and Delete function

--- TaskForge-API/controllers/taskControler.js
+++ TaskForge-API/controllers/taskControler.js
@@ -32,3 +32,38 @@ const createTask = (req, res) => {
     res.status(201).ison(newTask);
 };
 
+// updating task(PUT)
+const updateTask = (req, res) =>{
+    const task = tasks.find(t => t.id === req.params.id);
+    if (!task) return res.status(404).json({ message: "TASK IS NOT FOUND"});
+    res.status(200).json(task)  
+
+    // object destructuring to only extract tite and completed from the object
+    const { title, completed } = req.body;
+    
+    // if title and completed values were not provided leave everything as it is but if provided update only those values, so it will update the feild ony if the client actualy sent it
+    if (title !== undefined) task.title = title;
+    if (completed !== undefined) task.completed = completed;
+
+    res.status(200).json(task);
+};
+
+// deleteTask (DELETE)
+const deleteTask = (req, res) => {
+    // find the task by id and if the id does not exist send a 404 status
+    const index = tasks.findIndex(t => t.id ===req.params.id);
+    if (index === -1) return res.status(404).json({ message: "TASK NOT FOUND" });
+
+    // it will remove one task from an array
+    tasks.splice(index, 1);
+    res.status(204).send();
+};
+
+module.exports = { 
+    getAllTasks, 
+    getTaskById, 
+    createTask, 
+    updateTask, 
+    deleteTask
+};
+
--- TaskForge-API/server.js
+++ TaskForge-API/server.js
@@ -11,8 +11,8 @@ app.use(express.json());
 // use the router for a routes with API
 app.use("/api", router);
 
-//  statrtring the server with a port 3500
-const PORT = 3500
+//  starting the server with a port 3500
+const PORT = 3500;
 
 app.listen(PORT, () =>{
     console.log(`this server is running on http://localhost:${PORT}/api/tasks`)
