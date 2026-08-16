4b9d931dc867667b92e1b58c2748dc8c3852acf0
lesedi <analystmodikwe18@gmail.com>
2026-08-16 07:20:11 +0200

    restructuring the fie and adding error handler

--- TaskForge-API/server.js
+++ TaskForge-API/server.js
@@ -6,17 +6,21 @@ const app = express();
 const router = require("./routes/taskRoutes");
 // logger middleware
 const logger = require("./middleware/logger");
+// errorHandler middleware
 errorHandler = require("./middleware/errorHandler");
 
 
-// static files
-app.use(express.static("public"));
+
 // parsing JSON
 app.use(express.json());
 // logger
 app.use(logger);
+// static files
+app.use(express.static("public"));
 // use the router for a routes with API
 app.use("/api", router);
+// errorhandler
+app.use(errorHandler);
 
 
 //  starting the server with a port 3500
