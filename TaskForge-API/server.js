2ff68588d1c07e84c2c96745416f06b1a4a13278
lesedi <analystmodikwe18@gmail.com>
2026-08-16 07:16:24 +0200

    structuring the server fie and adding the error handler

--- TaskForge-API/server.js
+++ TaskForge-API/server.js
@@ -4,19 +4,20 @@ const app = express();
 
 // for routes
 const router = require("./routes/taskRoutes");
+// logger middleware
+const logger = require("./middleware/logger");
+errorHandler = require("./middleware/errorHandler");
+
 
 // static files
 app.use(express.static("public"));
-
 // parsing JSON
 app.use(express.json());
-
-// logger middleware
-const logger = require("./middleware/logger");
+// logger
 app.use(logger);
-
 // use the router for a routes with API
 app.use("/api", router);
+
 
 //  starting the server with a port 3500
 const PORT = 3500;
